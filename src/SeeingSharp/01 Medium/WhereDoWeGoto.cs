0064a930827ac4207181e13e1b0791c829d9f209
Stefan Daugaard Poulsen <cyberzed@sleddog.dk>
2014-07-03 21:41:13 +0200

    Goto away....

---
+++ src/SeeingSharp/01 Medium/WhereDoWeGoto.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+
+namespace SeeingSharp
+{
+    public class WhereDoWeGoto
+    {
+        public static void HaveFun()
+        {
+            var x = 0;
+
+            Loop:
+
+            if (x == 2)
+            {
+                goto Exit;
+            }
+            else
+            {
+                goto INCR;
+            }
+
+            Ups:
+            x = 1;
+
+            INCR:
+            x++;
+            goto Loop;
+
+            Exit:
+            ;
+        }
+    }
+}
